090956bf7498af4d0dec2a2bcba17cfd5b959627
sumin <cleonn7878@gmail.com>
2025-01-09 11:07:55 +0900

    Refactor: 팀 활동 리팩터링

--- W1/M3/m3_team_activity.py
+++ W1/M3/m3_team_activity.py
@@ -11,131 +11,139 @@ class Mode(Enum):
     TRANSFORM = 'TRANSFORM'
     LOAD = 'LOAD'
 
-def write_log(state: Mode, is_start: bool):
-    with open('./W1/M3/data/team_etl_project_log.txt', 'a') as log:
+state = Mode.EXTRACT
+gdp_url = "https://www.imf.org/external/datamapper/api/v1/NGDPD?periods=2025,2025"
+group_url = "https://www.imf.org/external/datamapper/api/v1/groups"
+region_url = "https://www.imf.org/external/datamapper/api/v1/regions"
+
+path = "./W1/M3/data/"
+db_name = "team_World_Economies.db"
+region_data_name = "region.csv"
+log_name = "team_etl_project_with_sql_log.txt"
+gdp_json_name = "team_gdp.json"
+region_json_name = "team_region.json"
+group_json_name = "team_group.json"
+
+def write_log(state: Mode, is_start: bool=True, is_error: bool=False, msg: Exception=None):
+    with open(path + log_name, 'a') as log:
         time = dt.datetime.now().strftime('%Y-%b-%d-%H-%M-%S')
-        if is_start:
+        if is_error:
+            log.write(f'{time}, [{state.value}] Failed\n\t{msg}\n')
+        elif is_start:
             log.write(f'{time}, [{state.value}] Started\n')
         else:
             log.write(f'{time}, [{state.value}] Ended\n')
-    log.close()
+
+def extract_data(url: str, f_name:str):
+    try:
+        ## API에서 데이터 가져오기
+        data = requests.get(url)
+        _save_to_json(f_name, data.text)
+    except Exception as e:
+        write_log(state, is_error=True, msg=e)
+        exit(1)
+    return data.text
+    
+def _save_to_json(f_name:str, data:str):
+    # RAW data JSON 파일로 저장
+    with open(path + f_name, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, indent=4)
+
+def transform_data(gdp_data_str:str, group_data_str:str, region_data_str:str):
+    ## json 객체로 변환
+    gdp_json_obejct = json.loads(gdp_data_str)
+    group_json_object = json.loads(group_data_str)
+    region_json_object = json.loads(region_data_str)
+
+    ## API에서 받은 JSON 데이터를 DataFrame으로 변환
+    gdp_df = pd.DataFrame(gdp_json_obejct['values']['NGDPD'])
+    group_df = pd.DataFrame(group_json_object['groups'])
+    region_df = pd.DataFrame(region_json_object['regions'])
+
+    ## DataFrame을 목적에 맞게 수정
+    gdp_df = pd.melt(gdp_df)
+    group_df = pd.melt(group_df)
+    region_df = pd.melt(region_df)
+
+    ## 국가가 아닌 데이터 제거
+    gdp_df = gdp_df[~gdp_df['variable'].isin(group_df['variable'])]
+    gdp_df = gdp_df[~gdp_df['variable'].isin(region_df['variable'])]
+
+    gdp_df.rename(columns={'variable':'Country', 'value':'GDP_USD_billion'}, inplace=True)
+    ## 국가별 대륙 매칭
+    region_info_df = pd.read_csv(path + region_data_name)
+
+    gdp_df = gdp_df.merge(
+        region_info_df[['alpha-3','region']],
+        left_on='Country',
+        right_on='alpha-3',
+        how='left'
+        )
+    gdp_df.drop(columns='alpha-3', inplace=True)
+    gdp_df.sort_values(by='GDP_USD_billion',ascending=False, inplace=True)
+    gdp_df['GDP_USD_billion'] = round(gdp_df['GDP_USD_billion'], 2)
+    gdp_df.reset_index(inplace=True, drop=True)
+    return gdp_df
+
+def load_to_db(gdp_imf: pd.DataFrame):
+    try:
+        con = sqlite3.connect(path + db_name)
+        gdp_imf.to_sql('Countries_by_GDP',con, if_exists='replace')
+        con.close()
+    except Exception as e:
+        write_log(state, is_error=True, msg=e)
+        exit(1)
 
 def print_query_result(query:str):
-    con = sqlite3.connect('./W1/M3/data/team_World_Economies.db')
+    con = sqlite3.connect(path + db_name)
     cursor = con.cursor()
     data = cursor.execute(query).fetchall()
-    formatted_data = [
-    [f"{x:,.2f}" if isinstance(x, float) else x for x in row]
-    for row in data
-    ]
-    columns = [description[0] for description in cursor.description]
-    print(tabulate(formatted_data, headers=columns, tablefmt='grid'))
     con.close()
+    columns = [description[0] for description in cursor.description]
+    print(tabulate(data, headers=columns, tablefmt='grid', floatfmt='.2f'))
 
-# [EXTRACT]
-state = Mode.EXTRACT
-write_log(state, True)
+if __name__ == "__main__":
+    # [EXTRACT]
+    write_log(state, True)
+    gdp_data_str = extract_data(gdp_url, gdp_json_name)
+    group_data_str = extract_data(group_url, group_json_name)
+    region_data_str = extract_data(region_url, region_json_name)
+    write_log(state, False)
+    state = Mode.TRANSFORM
 
-## API에서 데이터 가져오기
-### GDP 데이터
-gdp_data = requests.get('https://www.imf.org/external/datamapper/api/v1/NGDPD?periods=2025,2025')
-### 국가 그룹 데이터
-group_data = requests.get('https://www.imf.org/external/datamapper/api/v1/groups')
-### 리전 데이터
-region_data = requests.get('https://www.imf.org/external/datamapper/api/v1/regions')
+    # [TRANSFORM]
+    write_log(state, True)
+    gdp_df = transform_data(gdp_data_str, group_data_str, region_data_str)
+    write_log(state, False)
+    state = Mode.LOAD
 
-## 원본 데이터 저장
-with open('./W1/M3/data/team_gdp.json','w', encoding='utf-8') as gdp_file:
-    json.dump(gdp_data.text, gdp_file, indent=4)
-gdp_file.close()
+    # [LOAD]
+    write_log(state, True)
+    load_to_db(gdp_df)
+    write_log(state, False)
 
-with open('./W1/M3/data/team_group.json','w', encoding='utf-8') as group_file:
-    json.dump(group_data.text, group_file, indent=4)
-group_file.close()
-
-with open('./W1/M3/data/team_region.json','w', encoding='utf-8') as region_file:
-    json.dump(region_data.text, region_file, indent=4)
-region_file.close()
-
-write_log(state, False)
-state = Mode.TRANSFORM
-
-# [TRANSFORM]
-write_log(state, True)
-
-## json 객체로 변환
-gdp_json_obejct = json.loads(gdp_data.text)
-group_json_object = json.loads(group_data.text)
-region_json_object = json.loads(region_data.text)
-
-## API에서 받은 JSON 데이터를 DataFrame으로 변환
-gdp_df = pd.DataFrame(gdp_json_obejct['values']['NGDPD'])
-group_df = pd.DataFrame(group_json_object['groups'])
-region_df = pd.DataFrame(region_json_object['regions'])
-
-## DataFrame을 목적에 맞게 수정(컬럼이름, 소수점)
-gdp_df = pd.melt(gdp_df)
-gdp_df.rename(columns={'variable':'Country', 'value':'GDP_USD_billion'},inplace=True)
-gdp_df.sort_values(by='GDP_USD_billion',ascending=False, inplace=True)
-gdp_df['GDP_USD_billion'] = round(gdp_df['GDP_USD_billion'], 2)
-gdp_df.reset_index(inplace=True, drop=True)
-
-group_df = pd.melt(group_df)
-group_df.rename(columns={'variable':'group','value':'mean'}, inplace=True)
-
-region_df = pd.melt(region_df)
-region_df.rename(columns={'variable':'region','value':'mean'}, inplace=True)
-
-## 국가가 아닌 데이터 제거
-gdp_df = gdp_df[~gdp_df['Country'].isin(group_df['group'])]
-gdp_df = gdp_df[~gdp_df['Country'].isin(region_df['region'])]
-gdp_df.reset_index(inplace=True, drop=True)
-
-## 국가별 대륙 매칭
-region_info_df = pd.read_csv('./W1/M3/data/region.csv')
-gdp_df = gdp_df.merge(
-    region_info_df[['alpha-3','region']],
-    left_on='Country',
-    right_on='alpha-3',
-    how='left'
-    )
-gdp_df.drop(columns='alpha-3', inplace=True)
-
-write_log(state, False)
-state = Mode.LOAD
-
-# [LOAD]
-write_log(state, True)
-
-## DB에 DataFrame 저장
-con = sqlite3.connect('./W1/M3/data/team_World_Economies.db')
-gdp_df.to_sql('Countries_by_GDP',con, if_exists='replace')
-con.close()
-
-write_log(state, False)
-
-## [Query를 사용해 출력하기]
-print_query_result(
-    '''
-    SELECT *
-    FROM Countries_by_GDP
-    WHERE GDP_USD_billion > 100;
-    '''
-    )
-
-print_query_result(
-    '''
-    WITH rankedByRegionGdp AS (
-        SELECT
-            Country,
-            region,
-            GDP_USD_billion,
-            ROW_NUMBER() OVER (PARTITION BY region ORDER BY GDP_USD_billion DESC) AS rank
+    ## [Query를 사용해 출력하기]
+    print_query_result(
+        '''
+        SELECT *
         FROM Countries_by_GDP
+        WHERE GDP_USD_billion > 100;
+        '''
+        )
+
+    print_query_result(
+        '''
+        WITH rankedByRegionGdp AS (
+            SELECT
+                Country,
+                region,
+                GDP_USD_billion,
+                ROW_NUMBER() OVER (PARTITION BY region ORDER BY GDP_USD_billion DESC) AS rank
+            FROM Countries_by_GDP
+        )
+        SELECT region, AVG(GDP_USD_billion)
+        FROM rankedByRegionGdp
+        WHERE rank <= 5
+        GROUP BY region;
+        '''
     )
-    SELECT region, AVG(GDP_USD_billion)
-    FROM rankedByRegionGdp
-    WHERE rank <= 5
-    GROUP BY region;
-    '''
-)
